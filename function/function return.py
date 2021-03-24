01fa49fddbfba65c78a443a6324a516babf840f6
Yoon woo joung <ywj981217@naver.com>
2021-03-24 15:25:52 +0900

    2021-03-24

---
+++ function/function return.py
@@ -0,0 +1,15 @@
+# 함수를 만든다 - > 값을 더하여 반환해주는 함수
+
+def add(num1,num2): #함수의 인수를 두개만들어 두개를 더 해줘 반환한다.
+    return num1+num2   
+
+print(add(2,3),'입니다') # 호출 시 인수를 바꿔도  같은 명령을 실행한다.
+
+#곱셈
+
+def multipie(num1,num2):   #궁금증 : 함수의 인수가 2개로 지정되어 있는데 3가지를 곱하면 어떻게 되는가 / 오류 발생
+    return num1*num2        
+
+print(multipie(2,10),'입니다')
+
+print(multipie(2,5),'입니다')
