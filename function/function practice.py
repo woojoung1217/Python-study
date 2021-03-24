e0893f1a75a5d0f81ef99c2a79c225b37721842c
Yoon woo joung <ywj981217@naver.com>
2021-03-24 15:32:48 +0900

    2021-03-24

---
+++ function/function practice.py
@@ -0,0 +1,10 @@
+# 함수를 만들어 클로버 1~3 , 스페이드 1~3 , 하트 1~3 에게 유죄판결 문자를 출력하라
+
+def judge(name):
+    print(name,'1유죄판결')
+    print(name,'2유죄판결')
+    print(name,'3유죄판결')
+
+judge('클로버')
+judge('스페이드')
+judge('하트')
